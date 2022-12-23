da6cc05ceb864a4ff417ac55f290dc01eaba9ace
svan9 <75327234+svan9@users.noreply.github.com>
2022-12-23 21:39:41 +0300

    123

--- source/index.js
+++ source/index.js
@@ -14,21 +14,3 @@ $("textarea").each(function () {
         $("textarea").css("height", 0);
         $("textarea").css("height", (this.scrollHeight) + "px");
 });
-// $("textarea[name=in]").on("input", event => {
-//     event.preventDefault();
-//         $.ajax({
-//             global: false,
-//             type: 'POST',
-//             url: "/gettrans",
-//             dataType: 'html',
-//             data: {
-//                 value: $("textarea[name=in]").val()
-//             },
-//             success: (res, req) => {
-//                 $("textarea[name=out]").val(res);
-//             },  
-//             error: (request, error) => {
-
-//             }
-//         });
-// });
