964a9d935185126c1a44f43bece2862976e3482a
svan9 <75327234+svan9@users.noreply.github.com>
2022-12-23 21:44:47 +0300

    123

--- source/index.js
+++ source/index.js
@@ -1,8 +1,19 @@
-window.onload = () => {
+const fullcenterwidth = () => {
+    Array.from(document.body.children).forEach(e => {
+        if (e.tagName.toLowerCase() != "script")
+           if (e.getAttribute("center") != null) {
+                let halfElement = Number.parseInt($(e).css("width").replace("px", "")) / 2;
+                let clientWidth = document.body.clientWidth;
+                $(e).css({"left": (clientWidth / 2) - halfElement, "position": "absolute"});
+           }
+    });
 }
-update((e) => {
+
+const func = () => {
     fullcenterwidth();
-}); 
+};
+func();
+window.addEventListener("resize", e => func());
 
 getrs("./source/words.csv", data => {
     console.log(data);
@@ -14,3 +25,60 @@ $("textarea").each(function () {
         $("textarea").css("height", 0);
         $("textarea").css("height", (this.scrollHeight) + "px");
 });
+
+let getrs = (url, success) => {
+    $.ajax({
+      url: url,
+      dataType: 'text',
+      success: (data) => {
+        success(data);
+      }
+    });
+}
+
+
+
+
+// cookie
+let getCookie = name => {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+let stringToHTML = function (str) {
+	var dom = document.createElement('div');
+	dom.innerHTML = str;
+	return dom;
+};
+
+let setCookie = (name, value, options = {}) => {
+
+    options = {
+      path: '/',
+      // при необходимости добавьте другие значения по умолчанию
+      ...options
+    };
+  
+    if (options.expires instanceof Date) {
+      options.expires = options.expires.toUTCString();
+    }
+  
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+  
+    for (let optionKey in options) {
+      updatedCookie += "; " + optionKey;
+      let optionValue = options[optionKey];
+      if (optionValue !== true) {
+        updatedCookie += "=" + optionValue;
+      }
+    }
+  
+    document.cookie = updatedCookie;
+}
+
+let deleteCookie = name => {
+    setCookie(name, "", {
+        'max-age': -1
+    })
+}
